a3b61d6842fa6b6922cf7c81296d038aa9f4f771
Come Chabaille <comedauvignybcn@gmail.com>
2020-06-10 15:53:01 +0200

    finish database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurants = Restaurant.create([{ name: 'Star Wars' }, { address: 'carrer' }, { phone_number: '00000' }, { category: 'italian' }])
+Restaurant.create([{ name: 'Sickkkk' }, { address: 'calle mallorca' }, { phone_number: '000001' }, { category: 'belgian' }])
+Restaurant.create([{ name: 'yup' }, { address: 'calle corsega' }, { phone_number: '000002' }, { category: 'chinese' }])
+Restaurant.create([{ name: 'heyyyyy' }, { address: 'calle beer' }, { phone_number: '000003' }, { category: 'japanese' }])
+Restaurant.create([{ name: 'beer pong bar' }, { address: 'calle borracha' }, { phone_number: '000003' }, { category: 'italian' }])
+
+
+
